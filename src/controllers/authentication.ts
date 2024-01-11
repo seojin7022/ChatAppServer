8ba3efda8c977402a9c3875bac253d976f7092f3
seojin7022 <seojin7022@gmail.com>
2024-01-11 21:08:55 +0900

    Commit

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -28,7 +28,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
         await user.save();
 
-        res.cookie('JINI-AUTH', user.authentication.sessionToken, { domain: '34.22.75.122', path: '/', secure: false });
+        res.cookie('JINI-AUTH', user.authentication.sessionToken, { domain: '34.22.75.122', path: '/', secure: false }).send("cookie set");
         return res.status(200).json(user).end();
 
     } catch (error) {
