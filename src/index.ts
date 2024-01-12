d22bc61a253026019ed3ec48ad19caab72161530
seojin7022 <seojin7022@gmail.com>
2024-01-12 19:50:45 +0900

    Commit

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './routers/index'
+import fs from 'fs';
+import https from 'https';
+import path from 'path';
 
 const app = express();
 
@@ -20,10 +23,20 @@ app.use(bodyParser.json());
 
 
 
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, 'path/to/your/private.key')),
+//   cert: fs.readFileSync(path.join(__dirname, 'path/to/your/certificate.crt'))
+// };
+
+// https.createServer(options, app).listen(8080, () => {
+//     console.log('HTTPS server running on port 8080');
+//   });
+
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log('Server running on http://localhost:8080');
+server.listen(3000, () => {
+  console.log("Server lauched ✅");
+  
 })
 
 const MONGO_URL = 'mongodb+srv://seojin7022:seojin7022@cluster0.y7y5klt.mongodb.net/?retryWrites=true&w=majority'
@@ -33,3 +46,8 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
 app.use('/', router());
+app.use(express.static(path.join(process.cwd(), 'ChatApp')));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'ChatApp', 'index.html'))
+})
